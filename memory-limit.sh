f7533e7b88d96c67dddf7f73d92c252d08ff8ba5
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-02 21:57:15 +0800

    Update memory-limit.sh

--- memory-limit.sh
+++ memory-limit.sh
@@ -4,16 +4,12 @@ do
   mem_total=$(free | awk '/Mem/ {print $2}')
   mem_used=$(free | awk '/Mem/ {print $3}')
   mem_usage=$(echo "scale=2; $mem_used/$mem_total * 100.0" | bc)
-  if [ $(echo "$mem_usage < 15" | bc) -eq 1 ]; then
-    target_mem_usage=$(echo "scale=0; $mem_total * 0.15 / 1" | bc)
-    stress_mem=$(echo "$target_mem_usage - $mem_used" | bc) -b
-    stress --vm 1 --vm-bytes "${stress_mem}K" -b
-    cpulimit -e stress -l 30 -b
-    sleep 10
-  else
-    sleep 10
-    kill -9 $(pidof cpulimit)
-    cpulimit -e stress -l 30 -b
-    sleep 60
-  fi
+  target_mem_usage=$(echo "scale=0; $mem_total * 0.20 / 1" | bc)
+  stress_mem=$(echo "$target_mem_usage - $mem_used" | bc) -b
+  stress --vm 1 --vm-bytes "${stress_mem}K" --timeout 120
+  cpulimit -e stress -l 40 -b
+  sleep 120
+  kill -9 $(pidof cpulimit)
+  kill -9 $(jobs -p)
+  sleep 900
 done
