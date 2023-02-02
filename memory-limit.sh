793d1a6ae133374e959923903bbb0501104000e7
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-02 21:41:21 +0800

    Update memory-limit.sh

--- memory-limit.sh
+++ memory-limit.sh
@@ -7,7 +7,8 @@ do
   if [ $(echo "$mem_usage < 15" | bc) -eq 1 ]; then
     target_mem_usage=$(echo "scale=0; $mem_total * 0.15 / 1" | bc)
     stress_mem=$(echo "$target_mem_usage - $mem_used" | bc)
-    stress --vm 1 --vm-bytes "${stress_mem}K"
+    stress --vm 1 --vm-bytes "${stress_mem}K" 
+    cpulimit -e stress -l 30 -b
   else
     sleep 1
   fi
