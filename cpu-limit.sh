476755dd7bafc5f42869bd298e751288ce90835a
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-02 21:05:17 +0800

    Update cpu-limit.sh

--- cpu-limit.sh
+++ cpu-limit.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 CPU_USAGE=15
-CPUS=$(nproc)
-CPU_LIMIT=$(echo "$CPU_USAGE / $CPUS" | bc -l)
-for i in $(seq 1 "$CPUS"); do
-  cpulimit -l "$CPU_LIMIT" -b dd if=/dev/zero of=/dev/null &
+while true
+do
+  CPUS=$(nproc)
+  CPU_LIMIT=$(echo "$CPU_USAGE / $CPUS" | bc -l)
+  for i in $(seq 1 "$CPUS"); do
+    cpulimit -l "$CPU_LIMIT" -b dd if=/dev/zero of=/dev/null &
+  done
+  sleep 1800
+  kill $(jobs -p)
 done
