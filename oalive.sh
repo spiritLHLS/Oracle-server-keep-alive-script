ff0dc26fa01c7bb6dcba1b73508a046bc8ac0446
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-02 20:25:25 +0800

    Update oalive.sh

--- oalive.sh
+++ oalive.sh
@@ -55,7 +55,7 @@ calculate() {
 CPU_USAGE=15
 CPUS=$(nproc)
 DURATION=$(echo "100 / $CPU_USAGE * $CPUS" | bc)
-for i in $(seq 1 $CPUS); do
+for i in $(seq 1 "$CPUS"); do
   dd if=/dev/zero of=/dev/null &
 done
 sleep $DURATION
