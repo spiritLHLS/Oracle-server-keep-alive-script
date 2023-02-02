bcc5ce6a135ef37509772e7419fc6c5e10958dbc
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-02 20:56:53 +0800

    Update cpu-limit.sh

--- cpu-limit.sh
+++ cpu-limit.sh
@@ -3,7 +3,7 @@ CPU_USAGE=15
 CPUS=$(nproc)
 DURATION=$(echo "100 / $CPU_USAGE * $CPUS" | bc)
 for i in $(seq 1 "$CPUS"); do
-  dd if=/dev/zero of=/dev/null &
+  nice -n 19 ionice -c 3 dd if=/dev/zero of=/dev/null &
 done
 sleep $DURATION
 kill $(jobs -p)
