bc34120373692ad9f2fc366a10d2578ea0650934
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-02 22:37:34 +0800

    Update memory-limit.sh

--- memory-limit.sh
+++ memory-limit.sh
@@ -7,9 +7,9 @@ do
   target_mem_usage=$(echo "scale=0; $mem_total * 0.20 / 1" | bc)
   stress_mem=$(echo "$target_mem_usage - $mem_used" | bc) -b
   stress --vm 1 --vm-bytes "${stress_mem}K" --timeout 110
-  cpulimit -e stress -l 40 -b
-  sleep 130
-  kill -9 $(pidof cpulimit)
-  kill -9 $(jobs -p)
+#   cpulimit -e stress -l 40 -b
+#   sleep 130
+#   kill -9 $(pidof cpulimit)
+#   kill -9 $(jobs -p)
   sleep 900
 done
