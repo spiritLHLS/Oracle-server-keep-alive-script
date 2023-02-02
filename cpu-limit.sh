b4ccd2ba12267a097edd4fd2a6a322129b16e371
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-02 21:34:25 +0800

    Update cpu-limit.sh

--- cpu-limit.sh
+++ cpu-limit.sh
@@ -4,12 +4,11 @@ while true
 do
   CPUS=$(nproc)
   CPU_LIMIT=$(echo "$CPU_USAGE / $CPUS" | bc -l)
-  stress --cpu "$CPU_LIMIT" &
+  stress --cpu "$CPU_LIMIT"
   cpulimit -e stress -l "$CPU_LIMIT" -b
   sleep 60
   kill -9 $(pidof cpulimit)
   kill -9 $(jobs -p)
-#   kill -9 $(pidof stress)
   sleep 900
 done
 
