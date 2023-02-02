1ec4823fccf076b8cbf879bf04316f0592ae71f3
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-02 21:01:19 +0800

    Update cpu-limit.sh

--- cpu-limit.sh
+++ cpu-limit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CPU_USAGE=15
 CPUS=$(nproc)
-CPU_LIMIT=$(echo "15 / $CPUS" | bc -l)
+CPU_LIMIT=$(echo "$CPU_USAGE / $CPUS" | bc -l)
 for i in $(seq 1 "$CPUS"); do
   cpulimit -l "$CPU_LIMIT" -b dd if=/dev/zero of=/dev/null &
 done
