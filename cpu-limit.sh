357e332810cb6d25e458fdbecaec094a8662278f
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-03 21:07:54 +0800

    Update cpu-limit.sh

--- cpu-limit.sh
+++ cpu-limit.sh
@@ -17,7 +17,9 @@ function calculate_primes() {
 }
 
 size=350
-interval=10
+interval=5
+MIN_SIZE=100
+MIN_INTERVAL=3
 while true; do
   cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
   if (( $(echo "$cpu_usage < 15" | bc -l) )); then
@@ -26,16 +28,28 @@ while true; do
     else
       interval=$(echo "$interval - 0.5" | bc)
     fi
-    calculate_primes $size &
+    if [ $size -lt $MIN_SIZE ]; then
+      size=$MIN_SIZE
+    fi
+    if [ $(echo "$interval < $MIN_INTERVAL" | bc -l) -eq 1 ]; then
+      interval=$MIN_INTERVAL
+    fi
+    nice -n 19 calculate_primes $size &
   elif (( $(echo "$cpu_usage > 25" | bc -l) )); then
     if [ $((RANDOM % 2)) == 0 ]; then
       size=$((size-10))
     else
       interval=$(echo "$interval + 0.5" | bc)
     fi
-#     calculate_primes $size &
+    if [ $size -lt $MIN_SIZE ]; then
+      size=$MIN_SIZE
+    fi
+    if [ $(echo "$interval < $MIN_INTERVAL" | bc -l) -eq 1 ]; then
+      interval=$MIN_INTERVAL
+    fi
   else
     echo ""
   fi
   sleep $interval
 done
+
