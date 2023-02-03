abc6db10c6dc1879cfbfea6e3f59f564e706b343
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-03 11:13:34 +0800

    Update memory-limit.sh

--- memory-limit.sh
+++ memory-limit.sh
@@ -10,8 +10,8 @@ do
   if [ $(echo "$mem_usage < 25" | bc) -eq 1 ]; then
     target_mem_usage=$(echo "scale=0; $mem_total * 0.25 / 1" | bc)
     stress_mem=$(echo "$target_mem_usage - $mem_used" | bc)
-    timeout 60 memtester "${stress_mem}K" 1
+    timeout 60 stress-ng --vm 1 --vm-bytes "${stress_mem}K" --timeout 60s
   else
-    sleep 60
+    sleep 0.8
   fi
 done
