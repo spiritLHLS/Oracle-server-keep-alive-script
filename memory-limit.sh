5e1368da5c9682338223f8ac33797fca8843e9fe
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-03 11:03:06 +0800

    Update memory-limit.sh

--- memory-limit.sh
+++ memory-limit.sh
@@ -4,11 +4,11 @@ do
   mem_total=$(free | awk '/Mem/ {print $2}')
   mem_used=$(free | awk '/Mem/ {print $3}')
   mem_usage=$(echo "scale=2; $mem_used/$mem_total * 100.0" | bc)
-  if [ $(echo "$mem_usage < 25" | bc) -eq 1 ]; then
-    target_mem_usage=$(echo "scale=0; $mem_total * 0.25 / 1" | bc)
+  if [ $(echo "$mem_usage < 20" | bc) -eq 1 ]; then
+    target_mem_usage=$(echo "scale=0; $mem_total * 0.20 / 1" | bc)
     stress_mem=$(echo "$target_mem_usage - $mem_used" | bc)
     memtester "${stress_mem}K" 1
   else
-    sleep 0.8
+    sleep 30
   fi
 done
