0e15ba47b6a0f5fea95af6bb7faa65b081d3f300
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-26 23:28:24 +0800

    Update memory-limit.sh

--- memory-limit.sh
+++ memory-limit.sh
@@ -25,8 +25,8 @@ do
   if [ $(echo "$mem_usage < 25" | bc) -eq 1 ]; then
     target_mem_usage=$(echo "scale=0; $mem_total * 0.25 / 1" | bc)
     stress_mem=$(echo "$target_mem_usage - $mem_used" | bc)
-    stress_mem_in_gb=$(echo "scale=0; $stress_mem / 1024 / 1024" | bc)
-    fallocate -l "${stress_mem_in_gb}G" /dev/shm/file
+    stress_mem_in_gb=$(echo "scale=0; $stress_mem / 1024" | bc)
+    fallocate -l "${stress_mem_in_gb}M" /dev/shm/file
     sleep 300
     rm /dev/shm/file
   else
