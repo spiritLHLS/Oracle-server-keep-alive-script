772959252fa4cf88f107a7c26c552538110e9994
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-09-24 00:41:28 +0000

    2023.09.24.08.37

--- oalive.sh
+++ oalive.sh
@@ -2,7 +2,7 @@
 # by spiritlhl
 # from https://github.com/spiritLHLS/Oracle-server-keep-alive-script
 
-ver="2023.09.20.11.10"
+ver="2023.09.24.08.37"
 cd /root >/dev/null 2>&1
 _red() { echo -e "\033[31m\033[01m$@\033[0m"; }
 _green() { echo -e "\033[32m\033[01m$@\033[0m"; }
